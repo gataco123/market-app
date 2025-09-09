af45fefa9de5c0d19f89f2704dbf457fd4a30252
sebastian <sedadelo123@gmailcom>
2025-09-09 15:33:53 -0500

    sign in requery

---
+++ src/signup.php
@@ -0,0 +1,45 @@
+<?php
+    //Step 1 get database access
+
+    require('../config/database.php');
+    //Step 2 get form-data
+
+    $f_name    = $_POST['fname']; //los campos dentro de los corchetes 
+    $l_name    = $_POST['lname']; //deber ser igual al de el $
+    $m_number  = $_POST['mnumber'];
+    $id_number = $_POST['idnumber'];
+    $e_mail    = $_POST['email'];
+    $p_wd      = $_POST['passwd'];
+
+    //Step 3 create query to insert into
+    $query = "
+        INSERT INTO users (
+            firstname,
+            lastname,
+            mobile_number,
+            ide_number,
+            email,
+            password
+        )
+        Values(
+            '$f_name',
+            '$l_name',
+            '$m_number',
+            '$id_number',
+            '$e_mail',
+            '$p_wd'
+        )"
+    ;
+
+    //Step 4  execute query
+    $res = pg_query($conn,$query);
+
+
+    //step 5. validate result
+    if($res){
+        echo "user has been created succesfully !!!";
+    }else{
+        echo "something wrong!";
+    }
+
+?>
