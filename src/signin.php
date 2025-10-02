5733a4a2796c86a88f5a1b010a4bcc7eb16ef1b8
sebastian <sedadelo123@gmailcom>
2025-10-02 11:20:58 -0500

    validate login with MD5 and go to main page

--- src/signin.php
+++ src/signin.php
@@ -3,16 +3,19 @@
     require('../config/database.php');
 
     //Step 2 get form-data
-    $e_mail    = $_POST['email'];
-    $p_wd      = $_POST['passwd'];    
+    $e_mail    =trim($_POST['email']);
+    $p_wd      =trim($_POST['passwd']);    
 
-   $sql_check_user = "SELECT u.email, u.password FROM users u WHERE u.email = '$e_mail' AND u.password = '$p_wd' LIMIT 1";
+    //$enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+    $enc_pass = md5($p_wd);
 
-    // Ejecutar la consulta
-    $res = pg_query($conn, $sql_check_user);
+    $sql_check_user = "SELECT u.email, u.password FROM users u WHERE u.email = '$e_mail' AND u.password = '$enc_pass' LIMIT 1";
 
-    if(pg_num_rows($res) > 0){
-        echo "User exists, go to main page!";
+    $res_check = pg_query($conn, $sql_check_user);
+
+    if(pg_num_rows($res_check) > 0){
+        //echo "User exists, go to main page!";
+        header('refresh:0;url=main.php');
     } else {
         echo "Verify data";
     }
